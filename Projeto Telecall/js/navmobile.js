90cee080a6ae23100f391b5559ed16d18dcaed7d
Gabriel da Silva <115048603+vampbiel@users.noreply.github.com>
2025-06-17 17:21:30 -0300

    Delete Projeto Telecall directory

--- Projeto Telecall/js/navmobile.js
+++
@@ -1,8 +0,0 @@
-//código para criar um menu hamburguer para telas mobiles
-const hamburguer = document.querySelector(".hamburguer");
-const navMenu = document.querySelector(".menuMobile");
-
-hamburguer.addEventListener("click", () => {
-    hamburguer.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
